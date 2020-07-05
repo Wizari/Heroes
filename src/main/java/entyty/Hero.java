4ea6500b30db5997faab83f626dcf52abac94d72
wizari <git301398>
2020-07-05 13:05:34 +0300

    v0.0.1 refactor Hero

--- src/main/java/entyty/Hero.java
+++ src/main/java/entyty/Hero.java
@@ -9,11 +9,16 @@ public class Hero {
     }
 
     public void move(int distance) {
-        System.out.println(name+ ": Делаю шаг преред, дистанция до противника "+ distance + "м.");
+        System.out.println(name + ": Делаю шаг преред, дистанция до противника " + distance + "м.");
         if (distance == 1) {
-            System.out.println(name+ ": Ударяю противника");
-            System.out.println("Герой - "+ name+ " Побеждает");
+            hit();
         }
     }
+
+    void hit() {
+        System.out.println(name + ": Ударяю противника");
+        System.out.println("Герой - " + name + " Побеждает");
+    }
+
 
 }
